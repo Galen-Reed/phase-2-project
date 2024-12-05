15f2618ded95c524ba9ce1d8e1cab36c48306d88
GalenReed <galenreed@vt.edu>
2024-12-05 11:05:38 -0500

    added content to circuit page

--- src/pages/Circuits.jsx
+++ src/pages/Circuits.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
 
 function Circuits() {
 
+    const [circuitData, setCircuitData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/gp_circuits')
+        .then((response) => response.json())
+        .then((data) => setCircuitData(data))
+    },[])
 
 
     return (
@@ -10,9 +17,15 @@ function Circuits() {
             <header>
                 <NavBar />
             </header>
-            <main>
-                Circuits!
-            </main>
+            {
+                circuitData.map((circuit) => (
+                    <main>
+                        <h2>{circuit.name} Grand Prix</h2>
+                        <p>{circuit.location}</p>
+                        <a href={circuit.link} target="_blank">Learn more about the {circuit.name} Grand Prix</a>
+                    </main>
+                ))
+            }
         </div>
     )
 }
